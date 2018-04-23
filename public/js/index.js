ce6eca8c96c78d3bd4d7c20a240a785476becfb2
Your Name <trancongtr1071997@gmail.com>
2018-04-23 18:21:56 +0700

    a

--- public/js/index.js
+++ public/js/index.js
@@ -3,9 +3,9 @@ var socket = io();
 socket.on('connect', function () {
   console.log('Connected to server');
 
-  socket.emit('createEmail', {
-    to: 'jen@example.com',
-    text: 'Hey. This is Andrew.'
+  socket.emit('createMessage', {
+    from: 'Cong',
+    text: 'Hello Cong'
   });
 });
 
@@ -13,6 +13,6 @@ socket.on('disconnect', function () {
   console.log('Disconnected from server');
 });
 
-socket.on('newEmail', function (email) {
-  console.log('New email', email);
+socket.on('newMessage', function (message) {
+  console.log('New Message', message);
 });
--- server/server.js
+++ server/server.js
@@ -14,10 +14,20 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'Hang',
+    text: 'Hey Cong',
+    cretaeAt: 12333
+  });
+
   socket.emit('newEmail', {
     from: 'mike@example.com',
     text: 'Hey. What is going on.',
     createAt: 123
+  });
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
   });
 
   socket.on('createEmail', (newEmail) => {
