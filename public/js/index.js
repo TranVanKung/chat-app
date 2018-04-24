3981e6c0eb8825dd4c7d20d5275b237f4d5ae03d
Your Name <trancongtr1071997@gmail.com>
2018-04-24 23:56:16 +0700

    a

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: 'Cong',
-    text: 'Hello Cong'
-  });
 });
 
 socket.on('disconnect', function () {
--- server/server.js
+++ server/server.js
@@ -14,24 +14,20 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMessage', {
-    from: 'Hang',
-    text: 'Hey Cong',
-    cretaeAt: 12333
-  });
-
-  socket.emit('newEmail', {
-    from: 'mike@example.com',
-    text: 'Hey. What is going on.',
-    createAt: 123
-  });
-
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
-  });
 
-  socket.on('createEmail', (newEmail) => {
-    console.log('createEmail', newEmail);
+    // io.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
+
+    socket.broadcast.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
   });
 
   socket.on('disconnect', () => {
